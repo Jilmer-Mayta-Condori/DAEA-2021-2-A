7440436d8c26b3dd595e3a76f114958aff888d4f
Jilmer-Mayta-Condori <jilmer.mayta@tecsup.edu.pe>
2021-08-23 13:14:19 -0500

    lab1 parte 2 y reorganizacion de directorios

---
+++ lab1/prjLab01-1/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace prjLab01_1
+{
+    class Program
+    {
+        static void Raiz()
+        {
+            for(int a = 1; a <= 10; a++)
+            {
+                Console.WriteLine("La raiz cuadrada de {0} es {1}", a, Math.Sqrt(a));
+            }
+        }
+        static int Suma(int a, int b)
+        {
+            return a + b;
+        }
+        static void Main(string[] args)
+        {
+            Console.Title = "Procedimientos y funciones";
+            String opcion;
+            
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("[1] SUMA DE DOS NUMEROS");
+                Console.WriteLine("[2] IMPRIMIR LA RAIZ CUADRADA DE LOS PRIMEROS NUMEROS ENTEROS");
+                Console.WriteLine("[0] SALIR");
+                Console.WriteLine("ingrese una opcion y presione ENTER");
+                opcion = Console.ReadLine();
+                switch (opcion)
+                {
+                    case "1":
+                        Console.WriteLine("Ingrese el primer numero");
+                        int a = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("Ingrese el segundo valor");
+                        int b = Convert.ToInt32(Console.ReadLine());
+                        Console.WriteLine("La suma de {0} y {1} es {2}", a, b, Suma(a,b));
+                        Console.ReadKey();
+                        break;
+                    case "2":
+                        Console.WriteLine("Calculando....");
+                        Raiz();
+                        Console.ReadKey();
+                        break;
+                }
+            } while (!opcion.Equals("0"));
+
+        }
+    }
+}
