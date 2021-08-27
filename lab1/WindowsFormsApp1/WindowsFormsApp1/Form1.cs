d5972ee3f612a646b57f9ddc5d72e055c6a09c04
Jilmer-Mayta-Condori <jilmer.mayta@tecsup.edu.pe>
2021-08-26 22:07:35 -0500

    Laboratorio1 Interfaz parte1 - Diseño de Interfaz y Agregar datos en DataGridView

--- lab1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
+++ lab1/WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -16,5 +16,37 @@ namespace WindowsFormsApp1
         {
             InitializeComponent();
         }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label9_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            String nombre = txtNombre.Text;
+            String apellido = txtApellido.Text;
+            String dni = txtDNI.Text;
+            String telefono = txtTelefono.Text;
+            String email = txtEmail.Text;
+            String direccion = txtDireccion.Text;
+            String fecnac = dtpFechaNac.Text;
+            dGVUsuarios.Rows.Add("U10023", dni, nombre, apellido, direccion, telefono, email, fecnac);
+        }
     }
 }
