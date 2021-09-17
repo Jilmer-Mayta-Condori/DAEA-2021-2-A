5a5fd24b1a8159d4d9009f951ee4a654a09d3d2a
Jilmer-Mayta-Condori <68447830+Jilmer-Mayta-Condori@users.noreply.github.com>
2021-09-17 10:48:36 -0500

    Creacion de Todos los formularios del panel principal

--- lab2/Lab02-01/PrincipalMDI.cs
+++ lab2/Lab02-01/PrincipalMDI.cs
@@ -33,5 +33,82 @@ namespace Lab02_01
             frm.MdiParent = this;
             frm.Show();
         }
+
+        private void mnuManProductos_Click(object sender, EventArgs e)
+        {
+            ManProductos frm = new ManProductos();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuManCategorias_Click(object sender, EventArgs e)
+        {
+            ManCategorias frm = new ManCategorias();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuManPreveedores_Click(object sender, EventArgs e)
+        {
+            ManProveedores frm = new ManProveedores();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuManClientes_Click(object sender, EventArgs e)
+        {
+            ManClientes frm = new ManClientes();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuProcesoVenta_Click(object sender, EventArgs e)
+        {
+            ProcesoVentas frm = new ProcesoVentas();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuReporteVentas_Click(object sender, EventArgs e)
+        {
+            ProcesoCompras frm = new ProcesoCompras();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void reporteCompraToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            ReporteCompras frm = new ReporteCompras();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuReporteInventario_Click(object sender, EventArgs e)
+        {
+            ReporteInventario frm = new ReporteInventario();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuReporteProveedores_Click(object sender, EventArgs e)
+        {
+            ReporteProveedores frm = new ReporteProveedores();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuReporteUsuarios_Click(object sender, EventArgs e)
+        {
+            ReporteUsuarios frm = new ReporteUsuarios();
+            frm.MdiParent = this;
+            frm.Show();
+        }
+
+        private void mnuReporteClientes_Click(object sender, EventArgs e)
+        {
+            ReporteClientes frm = new ReporteClientes();
+            frm.MdiParent = this;
+            frm.Show();
+        }
     }
 }
--- lab2/Lab02-01/ReporteClientes.cs
+++ lab2/Lab02-01/ReporteClientes.cs
@@ -16,5 +16,15 @@ namespace Lab02_01
         {
             InitializeComponent();
         }
+
+        private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label6_Click(object sender, EventArgs e)
+        {
+
+        }
     }
 }
