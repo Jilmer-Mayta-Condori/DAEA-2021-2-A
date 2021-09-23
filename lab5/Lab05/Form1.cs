4b02813bead6834f7c5a0700daa6999668d25277
Jilmer-Mayta-Condori <68447830+Jilmer-Mayta-Condori@users.noreply.github.com>
2021-09-22 22:35:09 -0500

    Boton listar

--- lab5/Lab05/Form1.cs
+++ lab5/Lab05/Form1.cs
@@ -7,14 +7,42 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Data.SqlClient;
 
 namespace Lab05
 {
     public partial class Form1 : Form
     {
+
+        SqlConnection con;
+
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            String str = "Server=<DESKTOP-6TPRNVU\\LOCAL>;Database=School;Integrated Security=true";
+            con = new SqlConnection(str);
+        }
+
+        private void btnEliminar_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnListar_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            String sql = "SELECT * FROM Person";
+            SqlCommand cmd = new SqlCommand(sql,con);
+            SqlDataReader reader = cmd.ExecuteReader();
+
+            DataTable dt = new DataTable();
+            dt.Load(reader);
+
+            dgvListado.DataSource = dt;
         }
     }
 }
