8c2cc735c405b2abbc6145e46c3a1aaee53e476b
Jilmer-Mayta-Condori <68447830+Jilmer-Mayta-Condori@users.noreply.github.com>
2021-11-04 10:00:42 -0500

    Laboratorio parte 2 terminado

---
+++ lab11-B/SchoolApp/SchoolWeb/Default.aspx.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using SchoolBLL;
+using SchoolEntities;
+
+namespace SchoolWeb
+{
+    public partial class _Default : Page
+    {
+        CPersonBLL personBL = new CPersonBLL();
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            gvListado.DataSource = personBL.ListarBLL();
+            gvListado.DataBind();
+        }
+    }
+}
