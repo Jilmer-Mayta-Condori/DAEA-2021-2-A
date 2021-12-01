bf4d04b743d7aff29c89ea21adaadbcfbca54ddf
Jilmer-Mayta-Condori <68447830+Jilmer-Mayta-Condori@users.noreply.github.com>
2021-11-30 22:13:07 -0500

    Laboratorio terminado

---
+++ lab15/Lab15/Controllers/PersonController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using Lab15.Models;
+using Microsoft.Reporting.WebForms;
+using System.IO;
+using System.Web.Helpers;
+using System.Collections;
+
+namespace Lab15.Controllers
+{
+    public class PersonController : Controller
+    {
+        #region Contexto
+        private SchoolEntities _contexto;
+
+        public SchoolEntities Contexto
+        {
+            set { _contexto = value; }
+            get
+            {
+                if (_contexto == null)
+                    _contexto = new SchoolEntities();
+                return _contexto
+            }   
+        }
+        #endregion
+        // GET: Person
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
